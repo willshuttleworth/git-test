bdc290d7322ab206e3cca5d4487ef0a045b0c66c
Will Shuttleworth <willshuttleworth@ymail.com>
2022-03-07 14:06:16 -0500

    improved sum functionality by making list go from 1 to n

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-size = int(input('enter array size'))
+size = int(input('enter array size: '))
 nums = []
-for i in range(size):
+for i in range(1, size + 1):
 	nums.append(i)
 print(nums)
 
 sum = 0
 for i in nums:
-	sum = sum  + nums[i]
+	sum = sum  + i
 print('sum:', sum)
