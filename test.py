9398c4f0cf5d8344cb61ea8f593c3e925ba3cea7
Will Shuttleworth <willshuttleworth@ymail.com>
2022-03-07 13:20:37 -0500

    added array, printing array, and printing sum of array

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 name = input('enter your name: ')
 age = int(input('how old are you? '))
 print(name, 'is', age, 'years old')
+nums = []
+for i in range(10):
+	nums.append(i)
+print(nums)
+
+sum = 0
+for i in nums:
+	sum = sum  + nums[i]
+print('sum:', sum)
