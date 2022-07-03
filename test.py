45ab044b80797e4d0617c4880dc82a350d7583e9
Will Shuttleworth <93229335+willshuttleworth@users.noreply.github.com>
2022-07-03 18:11:41 -0400

    removed bloat from test.py

--- test.py
+++ test.py
@@ -4,10 +4,3 @@ for i in range(1, size + 1):
 	nums.append(i)
 print(nums)
 
-sum = 0
-for i in nums:
-	sum = sum  + i
-print('sum:', sum)
-print("did you know that this sum is equal to n(n+1)/2? (where n is the array size)")
-name = input("enter first name:")
-# added comment for commit
